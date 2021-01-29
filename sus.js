e722a0637d204a59e59659612d2095fd20ae3065
SudipWhiteHatJR <74854918+SudipWhiteHatJR@users.noreply.github.com>
2021-01-29 16:31:35 +0530

    Add files via upload

---
+++ sus.js
@@ -0,0 +1,28 @@
+class Sus {
+
+    constructor(body1,body2,offsetX,offsetY) {
+      this.offsetX = offsetX;
+      this.offsetY = offsetY ; 
+      
+        var options = {
+        bodyA:body1,
+        bodyB:body2,
+        pointB:{x:this.offsetX, y:this.offsetY} ,
+       }
+       this.Sus = Constraint.create(options);
+       World.add(world,this.Sus);
+
+    }
+    display () {
+
+        var pointA= this.Sus.bodyA.position;
+        var pointB= this.Sus.bodyB.position;
+        strokeWeight (1)
+        var Anchor1X = pointA.x
+        var Anchor1Y = pointA.y
+        var Anchor2X = pointB.x+this.offsetX
+        var Anchor2Y = pointB.y+this.offsetY
+        line (Anchor1X,Anchor1Y,Anchor2X,Anchor2Y);
+
+    }
+}
